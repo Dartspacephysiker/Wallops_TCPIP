bc22618adf19c5a21aed2cc24f86ba53e7bb9b57
DartSpacePhysiker <spencermarkhatch@gmail.com>
2014-08-15 13:51:59 -0400

    Wrote a script to make acquisition from TM1 much easier. It sends a small script to telnet with the appropriate channel names for each Dartmouth channel on TM1.

---
+++ Wallops_telnet_TM1.sh
@@ -0,0 +1,216 @@
+#!/usr/bin/bash
+
+
+TM1="192.168.2.201"
+TM1PORT="8999"
+PORT=${3:-5010}
+SLEEP="1"
+OUTDIR="/home/spencerh/data/CAPER/Wallops/post-vibe"
+FILE_PREFIX="_postvibe"
+
+print_defaults () {
+    echo "${0} <CHANNEL NAME> <MODE> <PORT (default: ${PORT})>"
+    echo ""
+    echo -e "MODES"
+    echo -e "====="
+    echo -e "\t0: Stop data transfer"
+    echo -e "\t1: Start transfer of MSB/LSB chans"
+    echo -e "\t2: Start transfer and acquisition of MSB/LSB chans"
+    echo -e "\t3: Start transfer, acquisition, and RTD of MSB/LSB chans"
+    echo -e "\t\tin ${OUTDIR}"
+    echo ""
+    echo -e "CHANNEL NAMES"
+    echo -e "============="
+    echo -e "\tDartmouth:"
+    echo -e "\t\tSKIN-{LO,HI}"
+    echo -e "\t\tELF-{A,B}{LO,HI}"
+    echo -e "\t\tVF-{A,B}{LO,HI}"
+    echo -e "\t\tVLF{A,B}"
+    echo -e "\t\tVLF{A,B}-AGC"
+    echo -e "\t\tHF-AGC"
+    echo -e "\tOslo:"
+    echo -e "\t\tLPC{1,2,3,4}"
+}
+
+telnet_start () {
+    echo "######################"
+    echo "#telnet ${TM1}#"
+    echo "######################"
+    {   #echo "listusedchs"; sleep ${SLEEP}; 
+	sleep $((SLEEP * 2));
+	echo "/stx preparetransfer"; sleep ${SLEEP}; 
+	echo "ch ${MSBCH}"; sleep ${SLEEP}; 
+	echo "ch ${LSBCH}"; sleep ${SLEEP}; 
+	echo "/etx"; sleep ${SLEEP};
+	echo "starttransfer ${PORT}"; sleep ${SLEEP};
+	#echo "exit"; sleep ${SLEEP};
+    } | tee >(telnet ${TM1} ${TM1PORT});
+
+
+}
+
+telnet_stop () {
+    echo "######################"
+    echo "#telnet ${TM1}#"
+    echo "######################"
+    {       sleep $((SLEEP * 2));
+	echo "stoptransfer"; sleep ${SLEEP};
+	echo "exit"; sleep ${SLEEP};
+    } | tee >(telnet ${TM1} ${TM1PORT});
+}
+
+case $1 in
+    "VLFA" | "VLF" ) 
+	MSBCH="26"; LSBCH="27";
+	echo "Selected VLFA...";   
+	PORT=5026;
+	ACQSZ=16384;
+        RTD="VLFA";
+        RTDSZ="2048";;
+    "VLFB" ) 
+	MSBCH="28"; LSBCH="29";
+	echo "Selected VLFB...";
+	PORT=5028;
+	ACQSZ=16384;
+	RTD="VLFB";
+        RTDSZ="2048";;
+    "VLFA-AGC" | "VLF-AGCA" | "VLFAGC" | "VLF-AGC" ) 
+	MSBCH="30"; LSBCH="31";
+	echo "Selected VLFA-AGC...";
+	PORT=5030;
+	ACQSZ=16384;
+	RTD="VLFAGC";
+        RTDSZ="2048";;
+    "VLFB-AGC" | "VLF-AGCB" )
+	MSBCH="32"; LSBCH="33";
+	echo "Selected VLFB-AGC...";
+	PORT=5032;
+	ACQSZ=16384;
+	RTD="VLFAGC";
+        RTDSZ="2048";;
+    "VF-ALO" | "VFALO" ) 
+	MSBCH="34"; LSBCH="35";
+	PORT=5034;
+	ACQSZ=8192;
+	RTD="VFALO";
+        RTDSZ=1024;
+	echo "Selected VF-ALO...";; 
+    "VF-AHI" | "VFAHI" | "VF" | "VFA" ) 
+	MSBCH="36"; LSBCH="37";
+	PORT=5036;
+	ACQSZ=8192;
+	RTD="VFAHI";
+        RTDSZ=1024;
+	echo "Selected VF-AHI...";;
+    "VF-BLO" | "VFBLO" ) 
+	MSBCH="38"; LSBCH="39";
+	PORT=5038;
+	ACQSZ=8192;
+	RTD="VFBLO";
+        RTDSZ=1024;
+	echo "Selected VF-BLO...";;
+    "VFB-HI" | "VFBHI" ) 
+	MSBCH="40"; LSBCH="41";
+	PORT=5040;
+	ACQSZ=8192;
+	RTD="VFBHI";
+        RTDSZ=1024;
+	echo "Selected VF-BHI...";;
+    "ELF-ALO" | "ELFALO" ) 
+	MSBCH="42"; LSBCH="43";
+	PORT=5042;
+	ACQSZ=4096;
+	RTD="ELFALO";
+        RTDSZ=1024;
+	echo "Selected ELF-ALO...";; 
+    "ELF-AHI" | "ELFAHI" ) 
+	MSBCH="44"; LSBCH="45";
+	PORT=5044;
+	ACQSZ=4096;
+	RTD="ELFAHI";
+        RTDSZ=1024;
+	echo "Using ELF-AHI...";;
+    "ELF-BLO" | "ELFBLO" ) 
+	MSBCH="46"; LSBCH="47";
+	PORT=5046;
+	ACQSZ=4096;
+	RTD="ELFBLO";
+        RTDSZ=1024;
+	echo "Selected ELF-BLO...";; 
+    "ELF-BHI" | "ELFBHI" ) 
+	MSBCH="48"; LSBCH="49";
+	PORT=5048;
+	ACQSZ=4096;
+	RTD="ELFBHI";
+        RTDSZ=1024;
+	echo "Selected ELF-BHI...";;
+    "SKIN-LO" | "SKINLO" ) 
+	MSBCH="50"; LSBCH="51";
+	PORT=5050;
+	ACQSZ=4096;
+	RTD="SKINLO";
+        RTDSZ=1024;
+	echo "Selected SKIN-LO...";; 
+    "SKIN-HI" | "SKINHI" | "SKIN" ) 
+	MSBCH="52"; LSBCH="53";
+	PORT=5052;
+	ACQSZ=4096;
+	RTD="SKINHI";
+        RTDSZ=1024;
+	echo "Selected SKIN-HI...";;
+    "HF-AGC" | "HFAGC" ) 
+	MSBCH="54"; LSBCH="55";
+	PORT=5054;
+	ACQSZ=4096;
+	RTD="HFAGC";
+        RTDSZ=1024;
+	echo "Selected HF-AGC...";; 
+    "LPC1" | "LPC" ) 
+	MSBCH="4"; LSBCH="5";
+	PORT=5064;
+	ACQSZ=4096;
+	RTD="LPC1";
+        RTDSZ=1024;
+	echo "Selected Oslo Langmuir Probe CH1...";; 
+    "LPC2" ) 
+	MSBCH="6"; LSBCH="7";
+	PORT=5066;
+	ACQSZ=4096;
+	RTD="LPC2";
+        RTDSZ=1024;
+	echo "Selected Oslo Langmuir Probe CH2...";; 
+    "LPC3" ) 
+	MSBCH="8"; LSBCH="9";
+	PORT=5068;
+	ACQSZ=4096;
+	RTD="LPC3";
+        RTDSZ=1024;
+	echo "Selected Oslo Langmuir Probe CH3...";; 
+    "LPC4" ) 
+	MSBCH="10"; LSBCH="11";
+	PORT=5070;
+	ACQSZ=4096;
+	RTD="LPC4";
+        RTDSZ=1024;
+	echo "Selected Oslo Langmuir Probe CH4...";; 
+    * )
+	echo -e "Invalid channel/no channel specified!\n"
+	print_defaults;
+	exit;;
+esac
+
+case $2 in
+    "0" )
+	telnet_stop;;
+    "1" )
+	telnet_start;;
+    "2" )
+	./tcp_player -p ${PORT} -P "TM1_${RTD}${FILE_PREFIX}" -g -r 6 -A ${ACQSZ} -R ${RTDSZ} -d 1 -m /tmp/rtd/rtd_tcp${RTD}.data -o ${OUTDIR} & telnet_start;;
+    "3" )
+	./tcp_player -p ${PORT} -P "TM1_${RTD}${FILE_PREFIX}" -g -r 6 -A ${ACQSZ} -R ${RTDSZ} -d 1 -m /tmp/rtd/rtd_tcp${RTD}.data -o ${OUTDIR} & telnet_start;
+	/usr/src/prtd/rtd_script.sh 1 tcp${RTD};;
+    * )
+	echo -e "Invalid mode/no mode given!\n";
+	print_defaults;;
+esac
+
