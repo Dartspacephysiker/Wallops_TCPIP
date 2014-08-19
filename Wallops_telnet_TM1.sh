00f30d7b2263818200d2dc649c0c52ee3ccca69f
DartSpacePhysiker <spencermarkhatch@gmail.com>
2014-08-19 16:14:02 -0400

    Updated Wallops_telnet_TM1 script

--- Wallops_telnet_TM1.sh
+++ Wallops_telnet_TM1.sh
@@ -4,12 +4,14 @@
 TM1="192.168.2.201"
 TM1PORT="8999"
 PORT=${3:-5010}
-SLEEP="1"
+SLEEP="2"
+ACQTIME=${4:-45}
+
 OUTDIR="/home/spencerh/data/CAPER/Wallops/post-vibe"
 FILE_PREFIX="_postvibe"
 
 print_defaults () {
-    echo "${0} <CHANNEL NAME> <MODE> <PORT (default: ${PORT})>"
+    echo "${0} <CHANNEL NAME> <MODE> <PORT (default: ${PORT})> <ACQUISITION TIME: (default:${ACQTIME} s)>"
     echo ""
     echo -e "MODES"
     echo -e "====="
@@ -43,6 +45,7 @@ telnet_start () {
 	echo "ch ${LSBCH}"; sleep ${SLEEP}; 
 	echo "/etx"; sleep ${SLEEP};
 	echo "starttransfer ${PORT}"; sleep ${SLEEP};
+	sleep ${ACQTIME};
 	#echo "exit"; sleep ${SLEEP};
     } | tee >(telnet ${TM1} ${TM1PORT});
 
@@ -61,138 +64,141 @@ telnet_stop () {
 
 case $1 in
     "VLFA" | "VLF" ) 
-	MSBCH="26"; LSBCH="27";
+	MSBCH="27"; LSBCH="28";
 	echo "Selected VLFA...";   
 	PORT=5026;
 	ACQSZ=16384;
         RTD="VLFA";
         RTDSZ="2048";;
     "VLFB" ) 
-	MSBCH="28"; LSBCH="29";
+	MSBCH="29"; LSBCH="30";
 	echo "Selected VLFB...";
 	PORT=5028;
 	ACQSZ=16384;
 	RTD="VLFB";
         RTDSZ="2048";;
     "VLFA-AGC" | "VLF-AGCA" | "VLFAGC" | "VLF-AGC" ) 
-	MSBCH="30"; LSBCH="31";
+	MSBCH="31"; LSBCH="32";
 	echo "Selected VLFA-AGC...";
 	PORT=5030;
 	ACQSZ=16384;
 	RTD="VLFAGC";
         RTDSZ="2048";;
     "VLFB-AGC" | "VLF-AGCB" )
-	MSBCH="32"; LSBCH="33";
+	MSBCH="33"; LSBCH="34";
 	echo "Selected VLFB-AGC...";
 	PORT=5032;
 	ACQSZ=16384;
 	RTD="VLFAGC";
         RTDSZ="2048";;
     "VF-ALO" | "VFALO" ) 
-	MSBCH="34"; LSBCH="35";
+	MSBCH="35"; LSBCH="36";
 	PORT=5034;
 	ACQSZ=8192;
 	RTD="VFALO";
         RTDSZ=1024;
 	echo "Selected VF-ALO...";; 
     "VF-AHI" | "VFAHI" | "VF" | "VFA" ) 
-	MSBCH="36"; LSBCH="37";
+	MSBCH="37"; LSBCH="38";
 	PORT=5036;
 	ACQSZ=8192;
 	RTD="VFAHI";
         RTDSZ=1024;
 	echo "Selected VF-AHI...";;
     "VF-BLO" | "VFBLO" ) 
-	MSBCH="38"; LSBCH="39";
+	MSBCH="39"; LSBCH="40";
 	PORT=5038;
 	ACQSZ=8192;
 	RTD="VFBLO";
         RTDSZ=1024;
 	echo "Selected VF-BLO...";;
     "VFB-HI" | "VFBHI" ) 
-	MSBCH="40"; LSBCH="41";
+	MSBCH="41"; LSBCH="42";
 	PORT=5040;
 	ACQSZ=8192;
 	RTD="VFBHI";
         RTDSZ=1024;
 	echo "Selected VF-BHI...";;
     "ELF-ALO" | "ELFALO" ) 
-	MSBCH="42"; LSBCH="43";
+	MSBCH="43"; LSBCH="44";
 	PORT=5042;
 	ACQSZ=4096;
 	RTD="ELFALO";
         RTDSZ=1024;
 	echo "Selected ELF-ALO...";; 
     "ELF-AHI" | "ELFAHI" ) 
-	MSBCH="44"; LSBCH="45";
+	MSBCH="45"; LSBCH="46";
 	PORT=5044;
 	ACQSZ=4096;
 	RTD="ELFAHI";
         RTDSZ=1024;
 	echo "Using ELF-AHI...";;
     "ELF-BLO" | "ELFBLO" ) 
-	MSBCH="46"; LSBCH="47";
+	MSBCH="47"; LSBCH="48";
 	PORT=5046;
 	ACQSZ=4096;
 	RTD="ELFBLO";
         RTDSZ=1024;
 	echo "Selected ELF-BLO...";; 
     "ELF-BHI" | "ELFBHI" ) 
-	MSBCH="48"; LSBCH="49";
+	MSBCH="49"; LSBCH="50";
 	PORT=5048;
 	ACQSZ=4096;
 	RTD="ELFBHI";
         RTDSZ=1024;
 	echo "Selected ELF-BHI...";;
     "SKIN-LO" | "SKINLO" ) 
-	MSBCH="50"; LSBCH="51";
+	MSBCH="51"; LSBCH="52";
 	PORT=5050;
 	ACQSZ=4096;
 	RTD="SKINLO";
         RTDSZ=1024;
 	echo "Selected SKIN-LO...";; 
     "SKIN-HI" | "SKINHI" | "SKIN" ) 
-	MSBCH="52"; LSBCH="53";
+	MSBCH="53"; LSBCH="54";
 	PORT=5052;
 	ACQSZ=4096;
 	RTD="SKINHI";
         RTDSZ=1024;
 	echo "Selected SKIN-HI...";;
     "HF-AGC" | "HFAGC" ) 
-	MSBCH="54"; LSBCH="55";
+	MSBCH="55"; LSBCH="56";
 	PORT=5054;
 	ACQSZ=4096;
 	RTD="HFAGC";
         RTDSZ=1024;
 	echo "Selected HF-AGC...";; 
     "LPC1" | "LPC" ) 
-	MSBCH="4"; LSBCH="5";
+	MSBCH="5"; LSBCH="6";
 	PORT=5064;
 	ACQSZ=4096;
 	RTD="LPC1";
         RTDSZ=1024;
 	echo "Selected Oslo Langmuir Probe CH1...";; 
     "LPC2" ) 
-	MSBCH="6"; LSBCH="7";
+	MSBCH="7"; LSBCH="8";
 	PORT=5066;
 	ACQSZ=4096;
 	RTD="LPC2";
         RTDSZ=1024;
 	echo "Selected Oslo Langmuir Probe CH2...";; 
     "LPC3" ) 
-	MSBCH="8"; LSBCH="9";
+	MSBCH="9"; LSBCH="10";
 	PORT=5068;
 	ACQSZ=4096;
 	RTD="LPC3";
         RTDSZ=1024;
 	echo "Selected Oslo Langmuir Probe CH3...";; 
     "LPC4" ) 
-	MSBCH="10"; LSBCH="11";
+	MSBCH="11"; LSBCH="12";
 	PORT=5070;
 	ACQSZ=4096;
 	RTD="LPC4";
         RTDSZ=1024;
 	echo "Selected Oslo Langmuir Probe CH4...";; 
+    "-h" | "--help" )
+	print_defaults;
+	exit;;
     * )
 	echo -e "Invalid channel/no channel specified!\n"
 	print_defaults;
@@ -207,8 +213,7 @@ case $2 in
     "2" )
 	./tcp_player -p ${PORT} -P "TM1_${RTD}${FILE_PREFIX}" -g -r 6 -A ${ACQSZ} -R ${RTDSZ} -d 1 -m /tmp/rtd/rtd_tcp${RTD}.data -o ${OUTDIR} & telnet_start;;
     "3" )
-	./tcp_player -p ${PORT} -P "TM1_${RTD}${FILE_PREFIX}" -g -r 6 -A ${ACQSZ} -R ${RTDSZ} -d 1 -m /tmp/rtd/rtd_tcp${RTD}.data -o ${OUTDIR} & telnet_start;
-	/usr/src/prtd/rtd_script.sh 1 tcp${RTD};;
+	./tcp_player -p ${PORT} -P "TM1_${RTD}${FILE_PREFIX}" -g -r 6 -A ${ACQSZ} -R ${RTDSZ} -d 1 -m /tmp/rtd/rtd_tcp${RTD}.data -o ${OUTDIR} & telnet_start & /usr/src/prtd/rtd_script.sh 1 tcp${RTD};;
     * )
 	echo -e "Invalid mode/no mode given!\n";
 	print_defaults;;
